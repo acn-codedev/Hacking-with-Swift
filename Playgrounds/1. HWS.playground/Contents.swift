4904cfc5cbb7e06eff500317f4638484be8a5a5b
acn-codedev <acn.codedev@gmail.com>
2024-01-01 21:51:38 +0530

    Initial commit

---
+++ Playgrounds/1. HWS.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
---
+++ Playgrounds/2. checkpoint_1.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+let temp_cel =  39.2
+let temp_fah = (temp_cel * 9.0/5.0) + 32.0
+print ("Temperature is \(temp_cel)° celsius which is equal to \(temp_fah)° fahrenheit")
